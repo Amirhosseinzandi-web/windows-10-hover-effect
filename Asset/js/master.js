c79c90a2b4990d4f00bfa0b40ea24c542a87dc31
Amirhosseinzandi-web <blackviper933@gmail.com>
2023-03-05 01:31:13 +0330

    new

--- Asset/js/master.js
+++ Asset/js/master.js
@@ -3,7 +3,7 @@ document.querySelectorAll("main>p").forEach((boxes) => {
     _rect = boxes.getBoundingClientRect();
     x = e.clientX - _rect.left;
     y = e.clientY - _rect.top;
-    boxes.style.borderImage = `radial-gradient(20% 75% at ${x}px ${y}px ,rgba(255,255,255,0.7),rgba(255,255,255,0.1) ) 1 / 1px / 0px stretch `;
+    boxes.style.borderImage = `radial-gradient(20% 75% at ${x}px ${y}px ,rgba(255,150,255,0.7),rgba(255,255,255,0.0) ) 1 / 1px / 0px stretch `;
   });
   boxes.addEventListener("mousemove", (e) => {
     _rect = boxes.getBoundingClientRect();
